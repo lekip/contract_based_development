66affb4a4c4c338d6d1ec2f9bd73185bd02f6d83
lekip <kasp964n@edu.eal.dk>
2017-02-06 15:01:21 +0100

    Implemented rule list

--- rock_scissor_paper_game/rock_scissor_paper_game/Program.cs
+++ rock_scissor_paper_game/rock_scissor_paper_game/Program.cs
@@ -6,10 +6,24 @@ using System.Threading.Tasks;
 
 namespace rock_scissor_paper_game
 {
+    public enum Weapon { scissor, rock, paper };
+
+    public class Rule
+    {
+        public Weapon weapon;
+        public Weapon defeats;
+
+        public Rule(Weapon weapon, Weapon defeats)
+        {
+            this.weapon = weapon;
+            this.defeats = defeats;
+        }
+    }
+
     public class Player
     {
         public String name;
-        public String value;
+        public Weapon value;
 
         public Player(String Name)
         {
@@ -22,51 +36,46 @@ namespace rock_scissor_paper_game
             Console.WriteLine("S) Scissors");
             Console.WriteLine("R) Rock");
             Console.WriteLine("P) Paper");
+            String k;
             do
             {
-                this.value = Console.ReadKey(true).KeyChar.ToString().ToUpper();
-            } while (this.value != "S" &&
-                    this.value != "R" &&
-                    this.value != "P");
+                k = Console.ReadKey(true).KeyChar.ToString().ToUpper();
+            } while (k != "S" &&
+                    k != "R" &&
+                    k != "P");
+            if (k == "S") { this.value = Weapon.scissor; };
+            if (k == "R") { this.value = Weapon.rock; };
+            if (k == "P") { this.value = Weapon.paper; };            
         }
 
-        public String compare(Player oppenent)
-        {
-
-            if (this.value == "S" && oppenent.value == "R")
-            {
-                return ("The winner is " + oppenent.name);
-            }
-            else if (this.value == "R" && oppenent.value == "P")
-            {
-                return ("The winner is " + oppenent.name);
-            }
-            else if (this.value == "P" && oppenent.value == "S")
-            {
-                return ("The winner is " + oppenent.name);
-            }
-
-            else if (this.value == "S" && oppenent.value == "P")
-            {
-                return ("The winner is " + this.name);
-            }
-            else if (this.value == "R" && oppenent.value == "S")
-            {
-                return ("The winner is " + this.name);
-            }
-            else if (this.value == "P" && oppenent.value == "R")
-            {
-                return ("The winner is " + this.name);
-            }
-            else
-            {
-                return "It's a tie";
-            }
-        }
     }
 
     class Program
     {
+        List<Rule> rules = new List<Rule>()
+        {
+            new Rule(Weapon.scissor, Weapon.paper),
+            new Rule(Weapon.paper, Weapon.rock),
+            new Rule(Weapon.rock, Weapon.scissor)
+        };
+
+
+        public String findWinner(Player player1, Player player2)
+        {
+            if (player1.value.Equals(player2.value))
+            {
+                return "draw";
+            };
+
+            foreach (Rule r in rules)
+            {
+                if (r.weapon.Equals(player1.value) && r.defeats.Equals(player2.value))
+                    return player1.name;
+            }
+
+            return player2.name;
+        }
+
         static void Main(string[] args)
         {
             Program myProgram = new Program();
@@ -87,10 +96,10 @@ namespace rock_scissor_paper_game
             Player player2 = new Player("Player 2");
             player2.enterValue();
             Console.Clear();
-            Console.WriteLine("Value of " + player1.name + ": " + player1.value);
-            Console.WriteLine("Value of " + player2.name + ": " + player2.value);
+            Console.WriteLine("Value of " + player1.name + ": " + player1.value.ToString());
+            Console.WriteLine("Value of " + player2.name + ": " + player2.value.ToString());
 
-            Console.WriteLine(player1.compare(player2));
+            Console.WriteLine(findWinner(player1,player2));
 
             Console.ReadLine();
         }
