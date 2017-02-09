ca6a710cec986d769a79d7fe48a258f0ac536d9e
lekip <kasp964n@edu.eal.dk>
2017-02-09 13:25:22 +0100

    Optimizations

--- rock_scissor_paper_game/rock_scissor_paper_game/Program.cs
+++ rock_scissor_paper_game/rock_scissor_paper_game/Program.cs
@@ -10,42 +10,26 @@ namespace rock_scissor_paper_game
 
     public class Weapon
     {
-        public HandType weapon;
-        public List<HandType> defeats;
+        public HandType AttackingHand;
+        public List<HandType> DefeatingOpponents;
 
-        public Weapon(HandType weapon, List<HandType> defeats)
+        public Weapon(HandType AttackingHand, List<HandType> DefeatingOpponents)
         {
-            this.weapon = weapon;
-            this.defeats = defeats;
+            this.AttackingHand = AttackingHand;
+            this.DefeatingOpponents = DefeatingOpponents;
         }
     }
 
     public class Player
     {
         public String name;
-        public HandType value;
+        public HandType playerWeapon;
 
         public Player(String Name)
         {
             this.name = Name;
         }
-
-        public void enterValue()
-        {
-            Console.WriteLine("Choose " + this.name + " value:");
-
-            int nr = 0;
-            var values = Enum.GetValues(typeof(HandType));
-            foreach (HandType weapon in values)
-            {
-                nr++;
-                Console.WriteLine(nr + ")" + weapon.ToString());
-            }
-            int sel = int.Parse(Console.ReadKey(true).KeyChar.ToString());
-            this.value = (HandType) (values.GetValue(sel - 1));
-            
-        }
-
+        
     }
 
     class Program
@@ -66,19 +50,52 @@ namespace rock_scissor_paper_game
             new Weapon(HandType.fire, new List<HandType>() { HandType.paper, HandType.rock, HandType.scissor}),
             new Weapon(HandType.waterbaloon, new List<HandType>() { HandType.fire}),
         };
+        
+        
+        public HandType enterValue()
+        /*
+
+        Precondition:
+
+        Postcondition:
+        Handtype selected
+        
+        */
+        {
+
+
+            int nr = 0;
+            var values = Enum.GetValues(typeof(HandType));
+            foreach (HandType weapon in values)
+            {
+                nr++;
+                Console.WriteLine(nr + ")" + weapon.ToString());
+            }
+
+            int sel = int.Parse(Console.ReadKey(true).KeyChar.ToString());
+            return (HandType)(values.GetValue(sel - 1));
+        }
 
 
         public String findWinner(Player player1, Player player2, List<Weapon> rules)
+        /*
+        Precondition: 
+        player1.value is initialized, player2.value is initialized
+        'rules' contains all rules
+
+        Postcondition:
+        Winner is found.
+        */
         {
-            if (player1.value.Equals(player2.value))
+            if (player1.playerWeapon.Equals(player2.playerWeapon))
             {
                 return "draw";
             };
 
             foreach (Weapon r in rules)
             {
-                if (r.weapon.Equals(player1.value) &&
-                    (r.defeats.Contains(player2.value)))
+                if (r.AttackingHand.Equals(player1.playerWeapon) &&
+                    (r.DefeatingOpponents.Contains(player2.playerWeapon)))
                     return player1.name;
             }
 
@@ -99,14 +116,16 @@ namespace rock_scissor_paper_game
         {
             Console.Clear();
             Player player1 = new Player("Player 1");
-            player1.enterValue();
+            Console.WriteLine("Choose " + player1.name + " value:");
+            player1.playerWeapon=enterValue();
             Console.Clear();
             Console.WriteLine();
             Player player2 = new Player("Player 2");
-            player2.enterValue();
+            Console.WriteLine("Choose " + player2.name + " value:");
+            player2.playerWeapon = enterValue();
             Console.Clear();
-            Console.WriteLine("Value of " + player1.name + ": " + player1.value.ToString());
-            Console.WriteLine("Value of " + player2.name + ": " + player2.value.ToString());
+            Console.WriteLine("Value of " + player1.name + ": " + player1.playerWeapon.ToString());
+            Console.WriteLine("Value of " + player2.name + ": " + player2.playerWeapon.ToString());
 
             Console.WriteLine(findWinner(player1,player2, defaultRules));
 
